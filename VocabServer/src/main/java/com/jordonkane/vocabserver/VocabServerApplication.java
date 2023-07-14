971747407c94d3a7bc8ce5c7f70149beded068ab
fatedwanderer <kanejord99@outlook.com>
2023-07-14 18:55:15 -0400

    init

---
+++ VocabServer/src/main/java/com/jordonkane/vocabserver/VocabServerApplication.java
@@ -0,0 +1,13 @@
+package com.jordonkane.vocabserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabServerApplication.class, args);
+	}
+
+}
---
+++ VocabServer/src/test/java/com/jordonkane/vocabserver/VocabServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordonkane.vocabserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
